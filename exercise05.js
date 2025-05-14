a5c47faab0ace86a9668aa61f0175366d31c8005
Aman Tripathi <123200498+aman73078@users.noreply.github.com>
2025-05-14 20:51:14 +0530

    21-24 completed

--- exercise05.js
+++ exercise05.js
@@ -76,12 +76,12 @@ const checkPositiveOrNegativeInteger = function(num1, num2) {
         if ((num1 > 0 && num2 < 0) || (num1 < 0 && num2 > 0)) {
             const positive = num1 > 0 ? num1 : num2;
             const negative = num1 < 0 ? num1 : num2;
-            console.log(`Positive: ${positive} and Negative: ${negative}`);
+            console.log(`Positive: ${positive} and Negative: ${negative}\n`);
         } else {
             console.log('No result — you entered wrong values (both numbers are positive or negative).');
         }
     } catch (error) {
-        console.log(`Error: ${error.message || error}`);
+        console.log(`Error: ${error.message || error}\n`);
     }
 };
 
@@ -89,3 +89,115 @@ checkPositiveOrNegativeInteger(50, -30);
 checkPositiveOrNegativeInteger(-50, 30);
 checkPositiveOrNegativeInteger(-50, -30);
 checkPositiveOrNegativeInteger(10, 20); 
+
+/* 21. Add 'Py' to Start of String if Not Present
+        Write a JavaScript program to create another string by adding "Py" in front of a given string. If the given string begins 
+        with "Py" return the original string. */
+const addPy=(inputString)=>{
+    try{
+        if(typeof inputString !== 'string'){
+            throw new Error(`Expected a string input`)
+        }
+        let result = inputString[0] + inputString[1] === 'Py' ? inputString : 'Py'+inputString;
+        console.log(`Result of addPy is : ${result}\n`);
+    }catch(error){
+        console.log(`Error of addPy : ${error.message || error}\n`);
+    }
+}
+addPy('rahul');
+addPy('Python');
+addPy(33);
+
+// 22. Remove Character at Specified Position in String
+//     Write a JavaScript program to remove a character at the specified position in a given string and return the modified string.
+const removeCharacter1 = function(position,givenString){
+    try{
+        let result = givenString.slice(0,position) + givenString.slice(position+1);
+        console.log(`Result of removeCharacter1 : ${result}\n`);
+    }catch(error){
+        console.log(`Error of removeCharacter1 : ${error.message || error}\n`);
+    }
+}
+removeCharacter1(2,'malhotra');
+removeCharacter1(4,'malhotra');
+
+const removeCharacter2 = function(position,givenString){
+    try{
+        let result = givenString.substring(0,position) + givenString.substring(position+1,givenString.length);
+        console.log(`Result of removeCharacter2 : ${result}\n`);
+    }catch(error){
+        console.log(`Error of removeCharacter2 : ${error.message || error}\n`);
+    }
+}
+removeCharacter2(3,'malhotra');
+removeCharacter2(2,'malhotra');
+removeCharacter2(4,'malhotra');
+
+/*  23. Swap First and Last Characters in String
+        Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters. The string length must be broader than or equal to 1. */
+function swapFirstToLast1(inputString){
+    try{
+        if(typeof inputString !== 'string'){
+            throw new Error('Expected a string input');
+        }
+        let clonedString = inputString;
+        let result = inputString[inputString.length-1] + clonedString.slice(1,inputString.length-1) + inputString[0]
+        console.log(`Result of swapFirstToLast : ${result}`);
+    }catch(error){
+        console.log(`Error of swapFirstToLast1 : ${error.message || error}`);
+    }
+}
+swapFirstToLast1('rahul');
+swapFirstToLast1('malhotra');
+swapFirstToLast1(3333);
+
+function swapFirstToLast2(inputString){
+    try{
+        if(typeof inputString !=='string'){
+            throw new Error('Expected a string input');
+        }
+        inputString = inputString.toLowerCase();
+        let result = inputString[inputString.length-1] + inputString.substring(1,inputString.length-1) + inputString[0]
+        console.log(`Result of swapFirstToLast2 : ${result}`);
+    }catch(error){
+        console.log(`Error of swapFirstToLast2: ${error.message}`);
+    }
+}
+swapFirstToLast2('rahul');
+swapFirstToLast2('malhotra');
+swapFirstToLast2(3333);
+
+/*  24. Add First Character to Front and Back of String
+        Write a JavaScript program to create another string from a given string with the first character of the given string added to the front and back. */
+
+const convertString1 =(inputString)=>{
+    try{
+        if(typeof inputString !== 'string'){
+            throw new Error('Expected a string input');
+        };
+        inputString = inputString.toLowerCase();
+        let result = inputString[0] + inputString + inputString[0];
+        console.log(`Result of convertString : ${result}`);
+    }catch(error){
+        console.log(`Error : ${error.message || error}`);
+    };
+};
+convertString1('rahul');
+convertString1('malhotra');
+
+const convertString2 = function(inputString){
+    try{
+        if(typeof inputString !== 'string'){
+            throw new Error('Expected a string input');
+        };
+        inputString = inputString.toLowerCase();
+        let result = inputString[0] + inputString.substring(0,inputString.length) + inputString[0];
+        console.log(`Result of convertString2 : ${result}`);
+    }catch(error){
+        console.log(`Error of converString2 : ${error.message || error}`);
+    }
+}
+convertString2('iphone');
+convertString2('microsoft');
+
+
